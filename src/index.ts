1eb9cc7bd08cfadc85e200d844bbbea65a8a842f
Chang <areschang114@qq.com>
2024-01-16 10:35:29 +0800

    chore: add cowsay package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import { say } from 'cowsay'
+
 export function AresChang() {
   console.log('Hello World!')
+  console.log('\x1B[31m%s\x1B[0m', '这是红色')
+  console.log('\x1B[36m%s\x1B[0m', '这是青色')
+
+  console.log(say({
+    text: '生而牛马，我很抱歉。',
+    e: 'oO',
+    T: 'U ',
+  }))
 }
+
+AresChang()
