24a7d200a3fb855c2a749408e56fe290c5632aae
Chang <areschang114@qq.com>
2024-01-16 13:39:52 +0800

    chore: add figlet package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { say } from 'cowsay'
 import colors from 'colors'
+import { textSync } from 'figlet'
+import { say } from 'cowsay'
 
 export function AresChang() {
-  console.log('Hello World!')
+  console.log(colors.rainbow(textSync('Ares Chang', 'Cola')))
+  console.log(colors.trap(textSync('Ares Chang')))
+  console.log(colors.rainbow(textSync('Hello World!', 'Ghost')))
+
   console.log(colors.rainbow('OMG Rainbows!')) // rainbow
   console.log(colors.trap('Run the trap')) // Drops the bass
 
