823b77a65fe9c81033e41718bc5a888d89533d94
Chang <areschang114@qq.com>
2024-01-20 18:15:18 +0800

    feat: 生成彩虹文字

--- src/index.ts
+++ src/index.ts
@@ -32,18 +32,33 @@ function findMe({ label, url }: FindMeItem) {
   return `${chalk.magenta(label)}: ${chalk.blue.underline(url)}`
 }
 
+/**
+ * 生成彩虹文字
+ * @param text
+ */
+function rainbow(text: string) {
+  const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
+
+  const list = text.split('').map((char, index) => {
+    const color = colors[index % colors.length]
+    return (chalk as any)[color](char)
+  })
+
+  return list.join('')
+}
+
 export function AresChang() {
-  log(textSync('Ares Chang'))
+  log(rainbow(textSync('Ares Chang')))
 
   log('可以在以下找到我:')
 
   list.forEach(obj => log(findMe(obj)))
 
-  log(say({
+  log(rainbow(say({
     text: '生而牛马，我很抱歉。',
     e: 'oO',
     T: 'U ',
-  }))
+  })))
 
   const blank = '                              '
   log(blank + chalk.red.underline('Happy every day!'))
